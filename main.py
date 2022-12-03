596352e3664bd335f9cdae62b6c9b1ca5c80b16a
MinhNhan <nvminhnhan@gmail.com>
2022-12-03 21:21:27 +0700

    first

--- main.py
+++ main.py
@@ -3,24 +3,22 @@ import numpy as np
 from imutils.video import VideoStream
 from yolodetect import YoloDetect
 
-video = VideoStream(src=1).start()
+video = VideoStream(src=0).start()
 # Chua cac diem nguoi dung chon de tao da giac
 points = []
 
 # new model Yolo
 model = YoloDetect()
 
-
 def handle_left_click(event, x, y, flags, points):
     if event == cv2.EVENT_LBUTTONDOWN:
         points.append([x, y])
 
-
-def draw_polygon (frame, points):
+def draw_polygon(frame, points):
     for point in points:
-        frame = cv2.circle( frame, (point[0], point[1]), 5, (0,0,255), -1)
+        frame = cv2.circle(frame, (point[0], point[1]), 5, (0, 0, 255), -1)
 
-    frame = cv2.polylines(frame, [np.int32(points)], False, (255,0, 0), thickness=2)
+    frame = cv2.polylines(frame, [np.int32(points)], False, (255, 0, 0), thickness=2)
     return frame
 
 detect = False
@@ -33,7 +31,7 @@ while True:
     frame = draw_polygon(frame, points)
 
     if detect:
-        frame = model.detect(frame= frame, points= points)
+        frame = model.detect(frame=frame, points=points)
 
     key = cv2.waitKey(1)
     if key == ord('q'):
--- telegram_utils.py
+++ telegram_utils.py
@@ -2,9 +2,9 @@ import telegram
 
 def send_telegram(photo_path="alert.png"):
     try:
-        my_token = "5404626616:AAGqQBl1bdUf0_cj2SQrFdZPiHoSy6VUVZg"
+        my_token = "5811414502:AAGx1n_NTi8ev93uD9cZLXLiRZYsvGJRB5s"
         bot = telegram.Bot(token=my_token)
-        bot.sendPhoto(chat_id="1246123900", photo=open(photo_path, "rb"), caption="Có xâm nhập, nguy hiêm!")
+        bot.sendPhoto(chat_id="1871882149", photo=open(photo_path, "rb"), caption="Có xâm nhập, nguy hiêm!")
     except Exception as ex:
         print("Can not send message telegram ", ex)
 
