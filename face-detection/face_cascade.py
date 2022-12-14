48406a5554eb0c6f0c3b6f7603967fec903283f0
MinhNhan <nvminhnhan@gmail.com>
2022-12-14 12:17:42 +0700

    Add face_detection

---
+++ face-detection/face_cascade.py
@@ -0,0 +1,49 @@
+import threading
+import datetime
+import cv2
+import yolodetect
+from telegram_utils import send_telegram
+
+# ------------------------------------------------------------------------------
+# automaticdai
+# YF Robotics Labrotary
+# Instagram: yfrobotics
+# Twitter: @yfrobotics
+# Website: https://www.yfrl.org
+# ------------------------------------------------------------------------------
+# Reference:
+# - https://towardsdatascience.com/face-detection-in-2-minutes-using-opencv-python-90f89d7c0f81
+# ------------------------------------------------------------------------------
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# To capture video from webcam.
+cap = cv2.VideoCapture(0)
+# To use a video file as input
+# cap = cv2.VideoCapture('filename.mp4')
+
+while True:
+    # Read the frame
+    _, img = cap.read()
+    img = cv2.flip(img, 1)
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Draw the rectangle around each face
+    label = "face_detected"
+    color = (0, 255, 0)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.putText(img, label, (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+    # Display
+    cv2.imshow('img', img)
+    # Stop if escape key is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+
+# Release the VideoCapture object
+cap.release()
+
